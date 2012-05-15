1cfbb0934b473467f7a858a638b9833385f5eda1
evenwu <evendesign@gmail.com>
2012-05-16 03:34:51 +0800

    init

---
+++ source/javascripts/all.js
@@ -0,0 +1,11 @@
+//= require jquery
+//= require fittext
+//= require_tree .
+
+$(document).ready(function() {
+
+$(".title").fitText(1.4);
+$(".sub-title").fitText(2);
+$(".big-title").fitText(0.6);
+
+});
