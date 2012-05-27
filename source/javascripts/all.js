45d14666728f4feaa0ddd63d7aee114daeed7db5
evenwu <evendesign@gmail.com>
2012-05-28 04:50:13 +0800

    完稿

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -7,9 +7,10 @@ $(".big-title").fitText(.9, { minFontSize: '53px', maxFontSize: '128px' });
 $(".sub-title").fitText(2.7, { minFontSize: '20px', maxFontSize: '43px' });
 $(".jf-title").fitText(2.7, { minFontSize: '30px', maxFontSize: '43px' });
 
-
 $(".equal-heights").equalHeights();
+$(".equal-heights2").equalHeights();
 
 $(window).resize(function(){
   $(".equal-heights").css("min-height","0").equalHeights();
+  $(".equal-heights2").css("min-height","0").equalHeights();
 });
